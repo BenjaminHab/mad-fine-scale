8f0583564140eb72bb863dd16447c275b25836c4
BenjaminHab <benjamin.hab@protonmail.com>
2025-08-10 12:15:17 +0200

    Used example to get mutual exclusive use of I2C working

--- software/mfs-rust/src/main.rs
+++ software/mfs-rust/src/main.rs
@@ -11,15 +11,19 @@ use embassy_stm32::time::Hertz;
 use embassy_stm32::Config;
 use embassy_stm32::{bind_interrupts, peripherals};
 use embassy_time::Timer;
-use embedded_hal_1::i2c::I2c as _;
+// use embedded_hal_1::i2c::I2c as _;
+use embedded_hal_async::i2c::I2c as _;
 use {defmt_rtt as _, panic_probe as _};
 
 use core::cell::RefCell;
-use embassy_embedded_hal::shared_bus::blocking::i2c::I2cDevice;
+use embassy_embedded_hal::shared_bus::asynch::i2c::I2cDevice;
 use embassy_sync::blocking_mutex::raw::NoopRawMutex;
 use embassy_sync::blocking_mutex::NoopMutex;
+use embassy_sync::mutex;
 // use embedded_hal_1::i2c::I2c as _;
 use static_cell::StaticCell;
+
+use embassy_stm32::pac::flash::Flash; // Maybe this allows for reading the VRefint factory calibration values at 0x1FFF 75AA - 0x1FFF 75AB
 
 bind_interrupts!(struct Irqs {
     I2C1 => i2c::EventInterruptHandler<peripherals::I2C1>, i2c::ErrorInterruptHandler<peripherals::I2C1>;
@@ -36,7 +40,9 @@ const read_wiper0: [u8; 1] = [rheo_volatile_wiper0_addr << 4 | rheo_read << 2]; 
 const increment_wiper0: [u8; 1] = [rheo_volatile_wiper0_addr << 4 | rheo_increment << 2];
 const decrement_wiper0: [u8; 1] = [rheo_volatile_wiper0_addr << 4 | rheo_decrement << 2];
 
-static I2C_BUS: StaticCell<NoopMutex<RefCell<I2c<'static, Async, i2c::Master>>>> = StaticCell::new();
+// static I2C_BUS: StaticCell<NoopMutex<RefCell<I2c<'static, Async, i2c::Master>>>> =
+//     StaticCell::new();
+type I2cBus = mutex::Mutex<NoopRawMutex, I2c<'static, Async, i2c::Master>>;
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
@@ -67,9 +73,18 @@ async fn main(_spawner: Spawner) {
     let _ = bridge_enable.set_low();
     debug!("GPIO setup complete!");
 
+    // Raw read from memory
+    const ADC: *mut u16 = (0x40012400) as *mut u16;
+    const ADC_DR: *mut u16 = ADC.wrapping_add(0x40);
+    const VREFINT: *mut u16 = (0x1FFF75AA) as *mut u16;
+    unsafe {
+        info!("VREFINT_LOW {}", core::ptr::read_volatile(VREFINT));
+        info!("ADC_DR {}", core::ptr::read_volatile(ADC_DR));
+    }
+
     let mut adc = Adc::new(p.ADC1);
-    adc.set_sample_time(SampleTime::CYCLES19_5);
-    adc.set_oversampling_ratio(0x03); // Oversampling ratio 16
+    adc.set_sample_time(SampleTime::CYCLES1_5);
+    adc.set_oversampling_ratio(0x03); // Oversampling ratio is 2^(x+1)
     adc.set_oversampling_shift(0b0000); // No bit shift after conversion, i.e. max value is 0xfff0
     adc.oversampling_enable(true);
 
@@ -78,8 +93,8 @@ async fn main(_spawner: Spawner) {
     let vrefint_sample = adc.blocking_read(&mut vrefint);
     info!("ADC reference value is {}", vrefint_sample);
     let convert_to_millivolts = |sample| {
-        // From stm32g0x1 datasheet 17.2: Reference voltage is 2.048V
-        const VREFINT_MV: u32 = 2048; // mV
+        // From stm32g0x0 datasheet: Reference voltage is 3V
+        const VREFINT_MV: u32 = 3000; // mV
 
         (u32::from(sample) * VREFINT_MV / u32::from(vrefint_sample)) as u16
     };
@@ -91,7 +106,7 @@ async fn main(_spawner: Spawner) {
     i2c_conf.scl_pullup = true;
     i2c_conf.timeout = embassy_time::Duration::from_millis(1000);
 
-    let mut i2c = I2c::new(
+    let i2c = I2c::new(
         p.I2C1,
         p.PA9,
         p.PA10,
@@ -102,9 +117,11 @@ async fn main(_spawner: Spawner) {
         i2c_conf,
     );
 
-    let i2c_bus = NoopMutex::new(RefCell::new(i2c));
-    let i2c_bus = I2C_BUS.init(i2c_bus);
-
+    // let i2c_bus = NoopMutex::new(RefCell::new(i2c));
+    // let i2c_bus = I2C_BUS.init(i2c_bus);
+    // let i2c_dev1 = I2cDevice::new(i2c_bus);
+    static I2C_BUS: StaticCell<I2cBus> = StaticCell::new();
+    let i2c_bus = I2C_BUS.init(mutex::Mutex::new(i2c));
     let mut i2c_dev1 = I2cDevice::new(i2c_bus);
 
     let mut write_wiper0: [u8; 2] = [rheo_volatile_wiper0_addr << 4 | rheo_write << 2, 0x00];
@@ -116,16 +133,18 @@ async fn main(_spawner: Spawner) {
         // for i in 0..12 {
         let i: u8 = 0;
         // Read rheo
-        match i2c_dev1.write_read(rheo_addr, &read_wiper0, &mut rx_buf) {
+        match i2c_dev1
+            .write_read(rheo_addr, &read_wiper0, &mut rx_buf)
+            .await
+        {
             Ok(_) => debug!("Read {:?} from wiper0", rx_buf[1]),
             Err(err) => debug!("error: {:?}", err),
         }
         Timer::after_millis(100).await;
-    
-        
+
         // Write rheo
         write_wiper0[1] = 10 * i; // set data byte
-        match i2c_dev1.write(rheo_addr, &write_wiper0) {
+        match i2c_dev1.write(rheo_addr, &write_wiper0).await {
             Ok(_) => debug!("Wrote {:?} to wiper0", &write_wiper0[1]),
             Err(err) => debug!("error: {:?}", err),
         }
@@ -153,15 +172,14 @@ async fn main(_spawner: Spawner) {
         // Gain factor = 1 + 100k/R_rheo; R_rheo is R_wiper (75R) + N/128 * 10k, N in 0..128
         // Gain should then be in 11..1334
         let mut v = adc.blocking_read(&mut pin);
-
-
+        info!("Library reading: {}", v);
 
         led.toggle();
         Timer::after_millis(100).await;
 
         // Autoscale reading
         while v < 3000 {
-            match i2c_dev1.write(rheo_addr, &decrement_wiper0) {
+            match i2c_dev1.write(rheo_addr, &decrement_wiper0).await {
                 Ok(_) => debug!("Decremented wiper0"),
                 Err(err) => debug!("error: {:?}", err),
             }
@@ -172,7 +190,7 @@ async fn main(_spawner: Spawner) {
         }
 
         while v > 50000 {
-            match i2c_dev1.write(rheo_addr, &increment_wiper0) {
+            match i2c_dev1.write(rheo_addr, &increment_wiper0).await {
                 Ok(_) => debug!("Incremented wiper0"),
                 Err(err) => debug!("error: {:?}", err),
             }
@@ -182,26 +200,26 @@ async fn main(_spawner: Spawner) {
             info!("--> {} - {} mV", v, convert_to_millivolts(v));
         }
 
-        let i2c_dev2 = I2cDevice::new(i2c_bus);
-        match get_gain(i2c_dev2).await {
+        match get_gain(i2c_bus).await {
             Ok(gain) => {
-                let reading = convert_to_millivolts(v);
-                info!("{}mV / {} = {}", reading, gain, (reading as f32)/gain);
-            },
-            Err(_) => {},
+                // let reading = convert_to_millivolts(v);
+
+                info!("{} / {} = {}", v, gain, (v as f32) / gain);
+            }
+            Err(_) => {}
         }
 
-        Timer::after_millis(5000).await;
+        Timer::after_millis(500).await;
 
         // }
     }
 }
 
-async fn get_gain(mut i2c: I2cDevice<'static, NoopRawMutex, I2c<'static, Async, i2c::Master>>) -> Result<f32, ()> {
+async fn get_gain(i2c_bus: &'static I2cBus) -> Result<f32, ()> {
     let mut data = [0u8; 2];
-
+    let mut i2c_dev = I2cDevice::new(i2c_bus);
     // FIXME: is it possible to get the write / read functions as async functions? Maybe that is the difference between blocking and non-blocking?
-    match i2c.write_read(rheo_addr, &read_wiper0, &mut data) {
+    match i2c_dev.write_read(rheo_addr, &read_wiper0, &mut data).await {
         Ok(_) => {
             let gain: f32 = 1.0f32 + (100.0e3f32 / (75.0f32 + (data[1] as f32) / 128.0f32));
             debug!("Calculated a gain of {}", gain);
